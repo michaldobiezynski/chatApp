59f50c9446c184df20e568076d72a56aaa0bf870
michaldobiezynski <michaldobiezynski@hotmail.com>
2020-09-20 19:27:01 +0100

    form and button states

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,28 +1,38 @@
 const socket = io();
 
+//Elements
+const $chatForm = document.querySelector("form");
+const $messageFormInput = $chatForm.querySelector("input");
+const $messageFormButton = $chatForm.querySelector("button");
+const $locationButton = document.querySelector("#send-location");
+
 socket.on("message", (message) => {
   console.log(message);
 });
 
-const chatForm = document.querySelector("form");
-
-chatForm.addEventListener("submit", (event) => {
+$chatForm.addEventListener("submit", (event) => {
   event.preventDefault();
+
+  $messageFormButton.setAttribute("disabled", "disabled");
 
   const messageData = event.target.elements.message.value;
   socket.emit("sendMessage", messageData, (error) => {
+    $messageFormButton.removeAttribute("disabled");
+    $chatForm.reset();
+    $messageFormInput.focus();
     if (error) {
       return console.log(error);
     }
 
     console.log("Message delivered!");
   });
-  chatForm.reset();
 });
 
-document.querySelector("#send-location").addEventListener("click", () => {
+$locationButton.addEventListener("click", () => {
+  $locationButton.setAttribute("disabled", "disabled");
   if (!navigator.geolocation) {
     return alert("Geolocation is not supported by your browser.");
+    $locationButton.removeAttribute("disabled");
   } else {
     navigator.geolocation.getCurrentPosition((position) => {
       console.log(position);
@@ -33,6 +43,7 @@ document.querySelector("#send-location").addEventListener("click", () => {
           lat: position.coords.latitude,
         },
         (message) => {
+          $locationButton.removeAttribute("disabled");
           console.log(message);
         }
       );
